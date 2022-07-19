1cdafa729332c8c98039bf93639bc3eba0fbc5e2
cavanosa <cavanosa@cavanosa.cav>
2022-07-19 21:42:35 +0200

    roles

--- src/main/java/com/tutorial/crudmongoback/security/controller/AuthController.java
+++ src/main/java/com/tutorial/crudmongoback/security/controller/AuthController.java
@@ -10,15 +10,13 @@ import com.tutorial.crudmongoback.security.service.UserEntityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/auth")
+@CrossOrigin
 public class AuthController {
 
     @Autowired
